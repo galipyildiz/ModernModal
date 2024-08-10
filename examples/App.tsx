58e3af4b44e366e9d41da1102314717a634031ac
galipyildiz <galipyildiz9606@outlook.com>
2024-08-10 19:55:16 +0300

    w'p

--- examples/App.tsx
+++ examples/App.tsx
@@ -2,12 +2,42 @@ import React, { useState } from "react";
 import { ModernModal } from "../src/components/ModernModal";
 
 const App = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isOpen, setIsOpen] = useState<boolean>(true);
 
   return (
     <div>
-      <button onClick={() => setIsOpen(true)}>Open Modal</button>
-      <ModernModal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+      <button onClick={() => (isOpen ? setIsOpen(false) : setIsOpen(true))}>
+        Open Modal
+      </button>
+      <ModernModal
+        size="xlarge"
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      >
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
+        <h1>Hello Modal!</h1>
         <h1>Hello Modal!</h1>
       </ModernModal>
     </div>
--- src/components/ModernModal.tsx
+++ src/components/ModernModal.tsx
@@ -1,22 +1,85 @@
-import React, { FC, SyntheticEvent } from "react";
+import React, {
+  CSSProperties,
+  FC,
+  SyntheticEvent,
+  useEffect,
+  useRef,
+} from "react";
 export interface ModernModalProps {
   isOpen: boolean;
   onClose: (
     event: SyntheticEvent<HTMLDialogElement, Event> | undefined
   ) => void;
   children: React.ReactNode;
+  size?: "xsmall" | "small" | "medium" | "large" | "xlarge" | "fullscreen";
 }
 export const ModernModal: FC<ModernModalProps> = ({
   isOpen,
   onClose,
   children,
+  size,
 }) => {
+  const modernModalRef = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      // prettier-ignore
+      if (modernModalRef.current && !modernModalRef.current.contains(event.target as Node)
+      ) {
+          onClose(undefined);
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
+
+  const getDialogStyles = (): CSSProperties => {
+    const baseStyles: CSSProperties = {
+      border: "2px solid red",
+      boxSizing: "border-box",
+      overflowY: "auto",
+      maxHeight: "calc(100vh - 20px)",
+    };
+    switch (size) {
+      case "fullscreen":
+        return {
+          ...baseStyles,
+          width: "100vw",
+          height: "100vh",
+          top: 0,
+          left: 0,
+        };
+      case "xlarge":
+        return {
+          ...baseStyles,
+          width: "calc(100vw - 128px)",
+          maxHeight: "calc(100vh - 128px)",
+        };
+      default:
+        return { ...baseStyles };
+    }
+  };
+
   return (
-    <dialog open={isOpen} onClose={onClose}>
-      <div>
-        <button onClick={() => onClose(undefined)}>Close</button>
-        {children}
-      </div>
-    </dialog>
+    isOpen && (
+      <dialog
+        ref={modernModalRef}
+        style={getDialogStyles()}
+        open={isOpen}
+        onClose={onClose}
+      >
+        <div>
+          <button onClick={() => onClose(undefined)}>Close</button>
+          {children}
+        </div>
+      </dialog>
+    )
   );
 };
